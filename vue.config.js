97b650bbc7384eae26b28693c1fbc29c60cd038b
Thorsten <t.luenborg@googlemail.com>
2018-12-26 22:44:52 +0100

    prepare alias to lib for demo app.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   lintOnSave: false,
+  chainWebpack: config => {
+    const path = require('path')
+    config.resolve.alias.set('vue-filedrop', path.join(__dirname, './src'))
+  },
 }
